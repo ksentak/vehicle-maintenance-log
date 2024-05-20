8ce82333fcf4269f77ad6426014da50188bf838d
Keaton Sentak <ksentak01@gmail.com>
2024-05-20 15:46:47 -0400

    load vehicle name and rename modal

--- src/views/MaintenanceLog.vue
+++ src/views/MaintenanceLog.vue
@@ -2,41 +2,69 @@
 import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import isString from 'lodash/isString';
-import get from 'lodash/get';
-import AddMaintenanceLogModal from '../components/AddMaintenanceLogModal.vue';
+import find from 'lodash/find';
+import MaintenanceLogModal from '../components/MaintenanceLogModal.vue';
 import MaintenanceLogCard from '../components/MaintenanceLogCard.vue';
 import BackBtn from '../components/BackBtn.vue';
 import Loader from '../components/Loader.vue';
 import useMaintenanceLogStore from '../stores/maintenanceLogStore';
+import useVehicleStore from '../stores/vehicleStore';
 import MaintenanceLog from '../interfaces/MaintenanceLog';
+import Vehicle from '../interfaces/Vehicle';
+
+const maintenanceLogs = ref<MaintenanceLog[]>([]);
+const vehicleInfo = ref<Vehicle>({
+  id: '',
+  make: '',
+  model: '',
+  year: 0,
+  vin: '',
+});
+const isLoading = ref<boolean>(false);
 
 const route = useRoute();
+const vehicleStore = useVehicleStore();
+
+const maintenanceLogStore = useMaintenanceLogStore();
+
 const vehicleId = isString(route.params.vehicleId)
   ? route.params.vehicleId
   : route.params.vehicleId[0];
-const maintenanceLogStore = useMaintenanceLogStore();
-const maintenanceLogs = ref<MaintenanceLog[]>([]);
-const isLoading = ref<boolean>(false);
 
-// Refactor
 const fetchMaintenanceLogs = async () => {
-  isLoading.value = true;
-  const logs = get(maintenanceLogStore.maintenanceLogs, vehicleId, []);
+  const logs = maintenanceLogStore.maintenanceLogs;
 
   if (!logs.length) {
     await maintenanceLogStore.fetchMaintenanceLogs(vehicleId);
   }
 
   maintenanceLogs.value = maintenanceLogStore.maintenanceLogs[vehicleId];
-  isLoading.value = false;
 };
 
-onMounted(fetchMaintenanceLogs);
+const getVehicleInfo = () => {
+  const foundVehicle = find(vehicleStore.vehicles, (vehicle: Vehicle) => {
+    return vehicle.id === vehicleId;
+  });
+
+  if (foundVehicle) {
+    vehicleInfo.value = foundVehicle;
+  }
+};
+
+onMounted(async () => {
+  isLoading.value = true;
+  getVehicleInfo();
+  await fetchMaintenanceLogs();
+  isLoading.value = false;
+});
 </script>
 
 <template>
   <div class="container mt-5">
-    <h1 class="text-center">Maintenance Log</h1>
+    <h1 class="text-center text-capitalize">
+      {{ vehicleInfo.year }} {{ vehicleInfo.make }}
+      {{ vehicleInfo.model }} Maintenance Logs
+    </h1>
     <div class="d-flex justify-content-between mb-3">
       <BackBtn />
       <button
@@ -45,7 +73,7 @@ onMounted(fetchMaintenanceLogs);
         data-bs-toggle="modal"
         data-bs-target="#addMaintenanceLogModal"
       >
-        Add Maintenance Log
+        Add Log
       </button>
     </div>
     <Loader v-if="isLoading" />
@@ -56,6 +84,6 @@ onMounted(fetchMaintenanceLogs);
         :log="log"
       />
     </div>
-    <AddMaintenanceLogModal :vehicleId="vehicleId" />
+    <MaintenanceLogModal :vehicleId="vehicleId" />
   </div>
 </template>
