1db018445834024f887c11abad8b5fba87a4cb18
Keaton Sentak <ksentak01@gmail.com>
2024-05-22 20:50:56 -0400

    fix add/edit modal

---
+++ src/components/MaintenanceLogForm.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+defineProps<{
+  isEditing: boolean;
+  logData: any;
+}>();
+</script>
+
+<template>
+  <form>
+    <div class="mb-3">
+      <label for="type" class="form-label">Type of Maintenance</label>
+      <input
+        type="text"
+        class="form-control"
+        id="type"
+        v-model="logData.type"
+        required
+      />
+    </div>
+    <div class="mb-3">
+      <label for="date" class="form-label">Date</label>
+      <input
+        type="date"
+        class="form-control"
+        id="date"
+        v-model="logData.date"
+        required
+      />
+    </div>
+    <div class="mb-3">
+      <label for="mileage" class="form-label">Vehicle Mileage</label>
+      <input
+        type="number"
+        class="form-control"
+        id="mileage"
+        v-model="logData.mileage"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="notes" class="form-label">Notes</label>
+      <textarea
+        class="form-control"
+        id="notes"
+        v-model="logData.notes"
+      ></textarea>
+    </div>
+  </form>
+</template>
--- src/components/MaintenanceLogModal.vue
+++ src/components/MaintenanceLogModal.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import Modal from './Modal.vue';
+import MaintenanceLogForm from './MaintenanceLogForm.vue';
 import useMaintenanceLogStore from '../stores/maintenanceLogStore';
 import MaintenanceLog from '../interfaces/MaintenanceLog';
 
@@ -44,47 +45,8 @@ const updateMaintenanceLog = async () => {
     :actionBtnText="'Update'"
     :submitFunc="updateMaintenanceLog"
   >
-    <form>
-      <div class="mb-3">
-        <label for="type" class="form-label">Type of Maintenance</label>
-        <input
-          type="text"
-          class="form-control"
-          id="type"
-          v-model="maintenanceLog.type"
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label for="date" class="form-label">Date</label>
-        <input
-          type="date"
-          class="form-control"
-          id="date"
-          v-model="maintenanceLog.date"
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label for="mileage" class="form-label">Vehicle Mileage</label>
-        <input
-          type="number"
-          class="form-control"
-          id="mileage"
-          v-model="maintenanceLog.mileage"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="notes" class="form-label">Notes</label>
-        <textarea
-          class="form-control"
-          id="notes"
-          v-model="maintenanceLog.notes"
-        ></textarea>
-      </div>
-    </form>
+    <MaintenanceLogForm :isEditing="isEditing" :logData="maintenanceLog" />
   </Modal>
-
   <Modal
     v-else
     :title="'Add Maintenance Log'"
@@ -92,44 +54,6 @@ const updateMaintenanceLog = async () => {
     :actionBtnText="'Save'"
     :submitFunc="addMaintenanceLog"
   >
-    <form>
-      <div class="mb-3">
-        <label for="type" class="form-label">Type of Maintenance</label>
-        <input
-          type="text"
-          class="form-control"
-          id="type"
-          v-model="maintenanceLog.type"
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label for="date" class="form-label">Date</label>
-        <input
-          type="date"
-          class="form-control"
-          id="date"
-          v-model="maintenanceLog.date"
-          required
-        />
-      </div>
-      <div class="mb-3">
-        <label for="mileage" class="form-label">Vehicle Mileage</label>
-        <input
-          type="number"
-          class="form-control"
-          id="mileage"
-          v-model="maintenanceLog.mileage"
-        />
-      </div>
-      <div class="mb-3">
-        <label for="notes" class="form-label">Notes</label>
-        <textarea
-          class="form-control"
-          id="notes"
-          v-model="maintenanceLog.notes"
-        ></textarea>
-      </div>
-    </form>
+    <MaintenanceLogForm :isEditing="false" :logData="maintenanceLog" />
   </Modal>
 </template>
